356a93b54dd817c2347a6628e994ec8f5495ff4c
hwanghyohyuk <hhhwang618@naver.com>
2017-09-04 10:33:18 +0900

    -

--- Workspace/productManager/src/product/view/ProductView.java
+++ Workspace/productManager/src/product/view/ProductView.java
@@ -11,8 +11,8 @@ import javax.swing.table.*;
 import product.controller.ProductController;
 import product.model.vo.Product;
 
-public class ProductView extends JFrame implements MouseListener{
-	
+public class ProductView extends JFrame {
+
 	private ProductController pController = new ProductController();
 
 	private JTable table;
@@ -22,20 +22,27 @@ public class ProductView extends JFrame implements MouseListener{
 	private JButton btn_viewList;
 	private JTextField tf_search;
 	private JButton btn_search;
-	private JTextField tf_pId;
 	private JTextField tf_pName;
 	private JSpinner sp_price;
 	private JTextArea ta_description;
 	private JButton btn_add;
 	private JButton btn_modify;
 	private JButton btn_delete;
+	private JLabel lbl_pIdInfo;
 
-	//private FileController fc = new FileController();
+	// private FileController fc = new FileController();
 	private ProductController pc = new ProductController();
 	private DefaultTableModel dtm = new DefaultTableModel(new Object[][] {},
-			new String[] { "product_id", "p_name", "price", "description" });
+			new String[] { "product_id", "p_name", "price", "description" }) { // 셀
+																				// 수정
+																				// 못하게
+																				// 하는
+																				// 부분
+		public boolean isCellEditable(int row, int column) {
+			return false;
+		}
+	};
 
-	
 	public ProductView() {
 		super("상품 관리 프로그램");
 		// 크기 설정
@@ -58,12 +65,14 @@ public class ProductView extends JFrame implements MouseListener{
 		this.addWindowListener(new WindowAdapter() {
 			@Override
 			public void windowOpened(WindowEvent e) {
-				pController.selectAll();
+				printList(pController.selectAll());
+				lbl_pIdInfo.setText(pController.getLastId());
+
 			}
 
 			@Override
 			public void windowClosing(WindowEvent e) {
-				//fc.fileSave(pc.demodeling(dtm));
+				// fc.fileSave(pc.demodeling(dtm));
 			}
 		});
 		// 좌측
@@ -83,7 +92,18 @@ public class ProductView extends JFrame implements MouseListener{
 		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		table.setModel(dtm);
 		scrollPane.setViewportView(table);
-		table.addMouseListener(this);
+		table.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				int row = table.getSelectedRow();
+				lbl_pIdInfo.setText((String) dtm.getValueAt(row, 0));
+				tf_pName.setText((String) dtm.getValueAt(row, 1));
+				sp_price.setValue(dtm.getValueAt(row, 2));
+				ta_description.setText((String) dtm.getValueAt(row, 3));
+			}
+		});
+		table.getTableHeader().setReorderingAllowed(false);
+		table.getTableHeader().setResizingAllowed(false);
 		// 우측
 
 		JPanel panel_East = new JPanel();
@@ -114,7 +134,7 @@ public class ProductView extends JFrame implements MouseListener{
 		btn_viewList.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-			//	table.setModel(pc.viewModel(dtm));
+				printList(pController.selectAll());
 			}
 		});
 		btn_viewList.setBounds(273, 10, 100, 25);
@@ -130,10 +150,14 @@ public class ProductView extends JFrame implements MouseListener{
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				if (rdbtn_pId.isSelected()) {
-					//table.setModel(pc.searchModel(true, tf_search.getText(), dtm));
+					System.out.println("상품 번호 : "+tf_search.getText()+" 검색");
+					printList(pController.selectId(tf_search.getText()));
+					
 				}
 				if (rdbtn_pName.isSelected()) {
-					//table.setModel(pc.searchModel(false, tf_search.getText(), dtm));
+
+					System.out.println("상품 명 : "+tf_search.getText()+" 검색");
+					printList(pController.selectName(tf_search.getText()));
 				}
 			}
 		});
@@ -141,7 +165,7 @@ public class ProductView extends JFrame implements MouseListener{
 		panel_Inner.add(btn_search);
 
 		JPanel panel_Inner2 = new JPanel();
-		panel_Inner2.setBounds(12, 148, 361, 403);
+		panel_Inner2.setBounds(12, 224, 361, 327);
 		panel_Inner.add(panel_Inner2);
 		panel_Inner2.setLayout(null);
 
@@ -156,43 +180,54 @@ public class ProductView extends JFrame implements MouseListener{
 
 		JLabel lbl_pId = new JLabel("상품 ID : ");
 		lbl_pId.setHorizontalAlignment(SwingConstants.CENTER);
-		lbl_pId.setBounds(12, 79, 85, 15);
+		lbl_pId.setBounds(12, 66, 85, 15);
 		panel_Inner2.add(lbl_pId);
 
 		JLabel lbl_pName = new JLabel("상 품 명 : ");
 		lbl_pName.setHorizontalAlignment(SwingConstants.CENTER);
-		lbl_pName.setBounds(12, 129, 85, 15);
+		lbl_pName.setBounds(12, 91, 85, 15);
 		panel_Inner2.add(lbl_pName);
 
 		JLabel lbl_price = new JLabel("가   격 : ");
 		lbl_price.setHorizontalAlignment(SwingConstants.CENTER);
-		lbl_price.setBounds(12, 179, 85, 15);
+		lbl_price.setBounds(12, 116, 85, 15);
 		panel_Inner2.add(lbl_price);
 
 		JLabel lbl_description = new JLabel("상품설명 : ");
 		lbl_description.setHorizontalAlignment(SwingConstants.CENTER);
-		lbl_description.setBounds(12, 229, 85, 15);
+		lbl_description.setBounds(12, 145, 85, 124);
 		panel_Inner2.add(lbl_description);
 
-		tf_pId = new JTextField();
-		tf_pId.setBounds(109, 76, 240, 21);
-		panel_Inner2.add(tf_pId);
-		tf_pId.setColumns(10);
+		lbl_pIdInfo = new JLabel("");
+		lbl_pIdInfo.setBounds(109, 66, 240, 15);
+		panel_Inner2.add(lbl_pIdInfo);
 
 		tf_pName = new JTextField();
-		tf_pName.setBounds(109, 126, 240, 21);
+		tf_pName.setBounds(109, 88, 240, 21);
 		panel_Inner2.add(tf_pName);
 		tf_pName.setColumns(10);
 
 		sp_price = new JSpinner();
 		sp_price.setModel(new SpinnerNumberModel(1000, 0, 100000000, 100));
-		sp_price.setBounds(190, 176, 159, 22);
+		sp_price.setBounds(109, 113, 240, 22);
 		panel_Inner2.add(sp_price);
 
 		JPanel panel_Inner4 = new JPanel();
-		panel_Inner4.setBounds(0, 373, 361, 30);
+		panel_Inner4.setBounds(0, 279, 361, 30);
 		panel_Inner2.add(panel_Inner4);
 		panel_Inner4.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+
+		JButton btn_new = new JButton("새로 쓰기");
+		btn_new.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				lbl_pIdInfo.setText(pController.getLastId());
+				tf_pName.setText("");
+				sp_price.setValue(1000);
+				ta_description.setText("");
+			}
+		});
+		panel_Inner4.add(btn_new);
 
 		btn_add = new JButton("추가");
 		panel_Inner4.add(btn_add);
@@ -202,39 +237,101 @@ public class ProductView extends JFrame implements MouseListener{
 		panel_Inner4.add(btn_delete);
 
 		JScrollPane scrollPane_1 = new JScrollPane();
-		scrollPane_1.setBounds(109, 229, 240, 124);
+		scrollPane_1.setBounds(109, 145, 240, 124);
 		panel_Inner2.add(scrollPane_1);
 
 		ta_description = new JTextArea();
 		scrollPane_1.setViewportView(ta_description);
 
+		JButton btn_descendingName = new JButton("Descending Name");
+		btn_descendingName.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				printList(pController.descendingName());
+			}
+		});
+		btn_descendingName.setBounds(12, 80, 175, 23);
+		panel_Inner.add(btn_descendingName);
+
+		JButton btn_ascendingName = new JButton("Ascending Name");
+		btn_ascendingName.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				printList(pController.ascendingName());
+			}
+		});
+		btn_ascendingName.setBounds(12, 113, 175, 23);
+		panel_Inner.add(btn_ascendingName);
+
+		JButton btn_descendingPrice = new JButton("Descending Price");
+		btn_descendingPrice.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				printList(pController.descendingPrice());
+			}
+		});
+		btn_descendingPrice.setBounds(199, 80, 174, 23);
+		panel_Inner.add(btn_descendingPrice);
+
+		JButton btn_ascendingPrice = new JButton("Ascending Price");
+		btn_ascendingPrice.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				printList(pController.ascendingPrice());
+			}
+		});
+		btn_ascendingPrice.setBounds(199, 115, 174, 23);
+		panel_Inner.add(btn_ascendingPrice);
+		
+		JButton btn_top3 = new JButton("Top 3 Price");
+		btn_top3.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				printList(pController.priceTop3());
+			}
+		});
+		btn_top3.setBounds(12, 146, 361, 32);
+		panel_Inner.add(btn_top3);
+
 		btn_add.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-			//	pc.addModel(tf_pId.getText(), tf_pName.getText(), (int) sp_price.getValue(), ta_description.getText(),
-			//			dtm);
-				tf_pId.setText("");
+
+				// pController.insertProduct 실행
+				pController.insertProduct(lbl_pIdInfo.getText(),tf_pName.getText(),(int)sp_price.getValue(),ta_description.getText());
+				printList(pController.selectAll());
+
+				lbl_pIdInfo.setText(pController.getLastId());
 				tf_pName.setText("");
-				sp_price.setValue(0);
+				sp_price.setValue(1000);
 				ta_description.setText("");
 			}
 		});
 		btn_modify.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				int row = table.getSelectedRow();
-		//		pc.modifyModel(row, tf_pId.getText(), tf_pName.getText(), (int) sp_price.getValue(),
-		//				ta_description.getText(), dtm);
+				// pController.updateProduct 실행.
+				pController.updateProduct(lbl_pIdInfo.getText(),tf_pName.getText(),(int)sp_price.getValue(),ta_description.getText());
+				printList(pController.selectAll());
+				
+				lbl_pIdInfo.setText(pController.getLastId());
+				tf_pName.setText("");
+				sp_price.setValue(1000);
+				ta_description.setText("");
 			}
 		});
 		btn_delete.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				int row = table.getSelectedRow();
-			//	pc.deleteModel(row, dtm);
-				tf_pId.setText("");
+
+				// pController.deleteProduct 실행
+				pController.deleteProduct((String)(table.getValueAt(row, 0)));
+				printList(pController.selectAll());
+				
+				lbl_pIdInfo.setText(pController.getLastId());
 				tf_pName.setText("");
-				sp_price.setValue(0);
+				sp_price.setValue(1000);
 				ta_description.setText("");
 			}
 		});
@@ -242,54 +339,27 @@ public class ProductView extends JFrame implements MouseListener{
 		setVisible(true);
 	}
 
-	@Override
-	public void mouseClicked(MouseEvent e) {
-		// TODO Auto-generated method stub
-		int row = table.getSelectedRow();
-		tf_pId.setText((String) dtm.getValueAt(row, 0));
-		tf_pName.setText((String) dtm.getValueAt(row, 1));
-		sp_price.setValue(Integer.parseInt((String) dtm.getValueAt(row, 2)));
-		ta_description.setText((String) dtm.getValueAt(row, 3));
-	}
-
-	@Override
-	public void mouseEntered(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void mousePressed(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void mouseReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	public void printAllList(ArrayList<Product> list) {
-		Iterator<Product> iter = list.iterator();
-		Object[][] oTemp = new Object[list.size()][4];
-		int i=0;
-		while(iter.hasNext()) {
-			Product product  = iter.next();
-		    oTemp[i][0]=product.getpId();
-		    oTemp[i][1]=product.getpName();
-		    oTemp[i][2]=product.getPrice();
-		    oTemp[i][3]=product.getDescription();
-		    i++;
-		}
-		for (int j = 0; j < oTemp.length; j++) {
-			dtm.addRow(oTemp[j]);
+	public void printList(ArrayList<Product> list) {
+		if (list != null) {
+			for (int k = dtm.getRowCount() - 1; k >= 0; k--) {
+				dtm.removeRow(k);
+			}
+			Iterator<Product> iter = list.iterator();
+			Object[][] oTemp = new Object[list.size()][4];
+			int i = 0;
+			while (iter.hasNext()) {
+				Product product = iter.next();
+				oTemp[i][0] = product.getpId();
+				oTemp[i][1] = product.getpName();
+				oTemp[i][2] = product.getPrice();
+				oTemp[i][3] = product.getDescription();
+				i++;
+			}
+			for (int j = 0; j < oTemp.length; j++) {
+				dtm.addRow(oTemp[j]);
+			}
+		} else {
+			System.out.println("결과가 존재하지 않습니다.");
 		}
 	}
 }
